ff892e8099799594870fe8ebac85687aaac743f2
hatom7 <2080096044@qq.com>
2025-07-06 18:22:15 +0800

    Add files via upload

---
+++ 机械臂控制/pinout.h
@@ -0,0 +1,60 @@
+#ifndef PINOUT_H_
+#define PINOUT_H_
+
+#define X_STEP_PIN         2
+#define X_DIR_PIN          5
+#define X_ENABLE_PIN       8
+#define X_MIN_PIN          9
+#define X_MAX_PIN          -1
+ 
+#define Y_STEP_PIN         3
+#define Y_DIR_PIN          6
+#define Y_ENABLE_PIN       8
+#define Y_MIN_PIN          10
+#define Y_MAX_PIN          -1
+
+#define Z_STEP_PIN         4
+#define Z_DIR_PIN          7
+#define Z_ENABLE_PIN       8
+#define Z_MIN_PIN          11
+#define Z_MAX_PIN          -1
+
+#define E0_STEP_PIN        12
+#define E0_DIR_PIN         13
+#define E0_ENABLE_PIN      8
+#define E0_MIN_PIN         -1
+
+#define E1_STEP_PIN        -1
+#define E1_DIR_PIN         -1
+#define E1_ENABLE_PIN      -1
+
+#define BYJ_PIN_0          -1
+#define BYJ_PIN_1          -1
+#define BYJ_PIN_2          -1
+#define BYJ_PIN_3          -1
+
+#define SERVO_PIN          A4
+
+#define PUMP_PIN           -1
+#define LASER_PIN          A5
+#define LED_PIN            -1
+
+#define PUMP1_PIN           A0
+#define PUMP2_PIN           A1
+#define PUMP3_PIN           A2
+#define PUMP4_PIN           A3
+
+#define SDPOWER            -1
+#define SDSS               -1
+
+#define FAN_PIN            -1
+
+#define PS_ON_PIN          -1
+#define KILL_PIN           -1
+
+//#define HEATER_0_PIN       10
+//#define HEATER_1_PIN        8
+#define TEMP_0_PIN         -1   // ANALOG NUMBERING
+#define TEMP_1_PIN         -1   // ANALOG NUMBERING
+
+#endif
